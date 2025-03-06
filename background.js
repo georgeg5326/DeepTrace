5bbc05225465a1de5db5ed3721c2d7e61b34aaa5
georgeg5326 <georgeg5326@gmail.com>
2025-03-05 20:16:21 -0500

    fixed background.js unable to store hostname

--- background.js
+++ background.js
@@ -1,20 +1,22 @@
-
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed');
 });
 
 chrome.webNavigation.onCompleted.addListener((details) => {
-    if(details.frameId === 0) { // check main frame
-        chrome.storage.local.get({vistedUrls: []}, (result) => {
-            const visitedUrls = result.vistedUrls;
-            visitedUrls.push(details.url);
-            chrome.storage.local.set({vistedUrls});
-        });
-    }
-}, {url: [{schemes: ['http', 'https']}]});
+  if (details.frameId === 0) { // check main frame
+    const url = new URL(details.url);
+    const hostname = url.hostname;
 
+    chrome.storage.local.get({ visitedUrls: {} }, (result) => {
+      let visitedUrls = result.visitedUrls || {};
+      if (visitedUrls[hostname]) {
+        visitedUrls[hostname]++;
+      } else {
+        visitedUrls[hostname] = 1;
+      }
+      chrome.storage.local.set({ visitedUrls });
+    });
+  }
+}, { url: [{ schemes: ['http', 'https'] }] });
 
-
-
-
-
+console.log("DeepTrace Tracking started...");
